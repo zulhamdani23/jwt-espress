45976f816bb51a8ab1830f45c6e41af442a6839b
zulhamdani23 <zulhamdani.mb@gmail.com>
2024-05-14 20:24:01 +0700

    delete some

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
 require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-var app = express();
-
+const express = require('express');
+const app = express();
 const port = process.env.PORT
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-// app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.listen(port, () => console.log(`Running di port ${port}`));
-
-//routes
 app.use('/api', require('./src/routes/route'));
-// app.use('/review', require('./src/routes/review'));
-// app.use('/user', require('./src/routes/users'));
-
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 module.exports = app;
